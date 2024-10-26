512d517ea11204660b218305b9a59f193ba45f74
helloRija <149434811+helloRija@users.noreply.github.com>
2024-10-26 10:56:42 -0400

    Update app.R

--- app.R
+++ app.R
@@ -1,39 +1,84 @@
 library(shiny)
 library(rsconnect)
-library(shinythemes)
 library(ggvis)
 library(cluster)
 library(cluster.datasets)  
+library(RCurl)
 
-OpenDef_Data_naRm <- read.csv(text = getURL("https://github.com/helloRija/RijaDSF01/blob/main/OpenDef_Data_naRm.csv")) 
+
+
+
+OpenDef_Data_naRm <- read.csv("OpenDef_Data_naRm.csv")
+
+###data(birth.death.rates.1966)
+##data(OpenDef_Data_naRm)
+###birth.death = birth.death.rates.1966
+birth.death = OpenDef_Data_naRm
+head(birth.death)
+
+### Load a sample data set
+
+### Remove the labels
+bd = birth.death[,-1]
+head(bd)
+
+### Select number of clusters
+###Total within cluster sum of squares (center=1)
+wss = kmeans(bd, centers=1)$tot.withinss
+
+####Find out SSE for clusters 2 to 15
+for (i in 2:15){
+  wss[i] = kmeans(bd, centers=i)$tot.withinss}
+
+###Plot SSE vs. Number of Clusters
+sse = data.frame(c(1:15), c(wss))
+names(sse)[1] = 'Clusters'
+names(sse)[2] = 'SSE'
+
+# sse %>%
+#   ggvis(~Clusters, ~SSE) %>%
+#   layer_points(fill := 'blue') %>% 
+#   layer_lines() %>%
+#   set_options(height = 300, width = 400)
+
+### Create Clusters
+clusters = kmeans(bd, 4)
+clusters
+
+### Assign Labels
+birth.death$Cluster = clusters$cluster
+head(birth.death)
+
+######Notsureif this is right for assigning values####
+birth.death$Cluster<- factor(birth.death$Cluster, levels = c("1", "2", "3", "4"))
 
 # ui.R
-library(shiny)
+
 ui = navbarPage("Rija's k-Means App",
-             tabPanel("Home",
-                      fluidPage(
-                        h1("Welcome to My App"),
-                        p("This Project is created by Rija Rochefort for
+                tabPanel("Home",
+                         fluidPage(
+                           h1("Welcome to My App"),
+                           p("This Project is created by Rija Rochefort for
                        DS501 – Introduction to Data Science
                        for Professor Narahara Chari Dingari, PhD
                       https://canvas.wpi.edu/courses/63229/assignments/368331
                       Fall 2024"),
-                        
-                        
-                        h2("Motivations"),
-                        p("My main motivations for this was because I had to. I didn't want to lose $5000, countless hours and have my life fall apart.
+                           
+                           
+                           h2("Motivations"),
+                           p("My main motivations for this was because I had to. I didn't want to lose $5000, countless hours and have my life fall apart.
                     I was surprised to find that I really enjoyed trying to make sense of this data, however the teaching aspect of it seems to be lacking. 
                     This seems like relatively complex coding without fully understanding how to code at this leve and the data Analysis. 
                     Therefore I felt the best way to do this project was to replicate something I had code for"),
-                        
-                        h2("Data collected"),
-                        p("In Week 7 of this class there was a lab on k-means clustering. I looked at the data and felt
+                           
+                           h2("Data collected"),
+                           p("In Week 7 of this class there was a lab on k-means clustering. I looked at the data and felt
                     it would be best to replicate this analysis. I found some really interesting data from Worldbank.org"
-                        ), 
-
-                        
-                        h2("Analysis"),
-                        p("I opted to do a Clustering analysis. I took a subset of data from WorldBank.org. 
+                           ), 
+                           
+                           
+                           h2("Analysis"),
+                           p("I opted to do a Clustering analysis. I took a subset of data from WorldBank.org. 
                 Health Nutritition and Population Statistics. I initially selected all countries. I chose 2019. The data bank
                 didn't have the most recent years. Because of Covid in 2020, I felt the data might not but fully reflective of my chosen categories. 
                 The cluster analysis focused on people practicing open defecation and deaths related to nutrition and communicable diseases. 
@@ -46,69 +91,69 @@ Create Clusters
 Assign Labels
 Plot Clusters
 Summary"),   
-                        
-                        h2("Findings")
+                           
+                           h2("Findings")
+                           
+                           
+                           
+                         ) ##Close fluid page
+                ),###closes name of panel
+                
+                ###SecondTab
+                tabPanel("Summary", 
                          
-                        
-                        
-                      ) ##Close fluid page
-            ),###closes name of panel
-             
-             ###SecondTab
-             tabPanel("Summary", 
-                      
-                      fluidPage(
-                        sidebarLayout(
-                          sidebarPanel(
-                            selectInput("cluster", label = "Cluster:", 
-                                        choices = birth.death$cluster,
-                                        selected = "All"),
-                            actionButton("update_plot", "Show Plot")
-                          ),
-                          mainPanel(
-                            plotOutput("plot")
-                          ))
-                      ) ##Close fluid page
-             ),###closes name of panel
-             
-             
-             ###ThirdTab
-             tabPanel("Summary", 
-                      
-                      fluidPage(
-                        sidebarLayout(
-                          sidebarPanel(
-                            selectInput("cluster", label = "Cluster:", 
-                                        choices = birth.death$Cluster,
-                                        selected = "All"),
-                            actionButton("update_plot", "Show Plot")
-                          ),
-                          mainPanel(
-                            plotOutput("plot")
-                          ))
-                      ) ##Close fluid page
-             ))###closes name of panel
-             
-             ###Plots
-             tabPanel("Plots", 
-                      
-                      fluidPage(
-                        sidebarLayout(
-                          sidebarPanel(
-                            selectInput("cluster", label = "Cluster:", 
-                                        choices = birth.death$cluster,
-                                        selected = "All"),
-                            actionButton("update_plot", "Show Plot")
-                          ),
-                          mainPanel(
-                            plotOutput("plot")
-                          ))
-                      ) ##Close fluid page
-             )###closes name of panel
-             
-             
+                         fluidPage(
+                           sidebarLayout(
+                             sidebarPanel(
+                               selectInput("cluster2", label = "Cluster:", 
+                                           choices = birth.death$Cluster,
+                                           selected = "All"),
+                               actionButton("update_plot2", "Show Plot")
+                             ),
+                             mainPanel(
+                               plotOutput("plot2")
+                             ))
+                         ) ##Close fluid page
+                ),###closes name of panel
+                
+                
+                ###ThirdTab
+                tabPanel("Summary", 
+                         
+                         fluidPage(
+                           sidebarLayout(
+                             sidebarPanel(
+                               selectInput("cluster3", label = "Cluster:", 
+                                           choices = birth.death$Cluster,
+                                           selected = "All"),
+                               actionButton("update_plot3", "Show Plot")
+                             ),
+                             mainPanel(
+                               plotOutput("plot3")
+                             ))
+                         ) ##Close fluid page
+                ))###closes name of panel
+
+###Plots
+tabPanel("Plots", 
+         
+         fluidPage(
+           sidebarLayout(
+             sidebarPanel(
+               selectInput("cluster", label = "Cluster:", 
+                           choices = birth.death$Cluster,
+                           selected = "All"),
+               actionButton("update_plot", "Show Plot")
+             ),
+             mainPanel(
+               plotOutput("plot")
+             ))
+         ) ##Close fluid page
+)###closes name of panel
+
+
 ### Close Sections
-  
+
 
 
 
@@ -117,54 +162,9 @@ server = shinyServer(function(input, output) {
   
   
   ####  Reactive expression for the plot
-  plot_data <- reactive({
-    if (input$update_plot) {
-      # Update data (e.g., filter or transform)
-      
-      ### Load a sample data set
-      library(cluster)
-      library(cluster.datasets)
-      
-      ###data(birth.death.rates.1966)
-      data(OpenDef_Data_naRm)
-      ###birth.death = birth.death.rates.1966
-      birth.death = OpenDef_Data_naRm
-      head(birth.death)
-      
-      ### Remove the labels
-      bd = birth.death[,-1]
-      head(bd)
-      
-      ### Select number of clusters
-      ###Total within cluster sum of squares (center=1)
-      wss = kmeans(bd, centers=1)$tot.withinss
-      
-      ####Find out SSE for clusters 2 to 15
-      for (i in 2:15)
-        wss[i] = kmeans(bd, centers=i)$tot.withinss
-    }
-    ###Plot SSE vs. Number of Clusters
-    library(ggvis)
-    sse = data.frame(c(1:15), c(wss))
-    names(sse)[1] = 'Clusters'
-    names(sse)[2] = 'SSE'
-    sse %>%
-      ggvis(~Clusters, ~SSE) %>%
-      layer_points(fill := 'blue') %>% 
-      layer_lines() %>%
-      set_options(height = 300, width = 400)
-    
-    ### Create Clusters
-    clusters = kmeans(bd, 4)
-    clusters
-    
-    ### Assign Labels
-    birth.death$Cluster = clusters$cluster
-    head(birth.death)
-    
-    ######Notsureif this is right for assigning values####
-    birth.death$Cluster<- factor(birth.death$Cluster, levels = c("1", "2", "3", "4"))
-    
+  plot_data2 <- reactive({
+    if (input$update_plot2) {
+  
     ### Plot Clusters
     #### customize clusplot
     plot_data <- clusplot(bd, clusters$cluster, 
@@ -173,12 +173,31 @@ server = shinyServer(function(input, output) {
                           main="k-Means Cluster Analysis", 
                           xlab = "Pop Practicing Open Defecation",
                           ylab = "Deaths by Conditions")
+    }
+  })
+  
+  plot_data3 <- reactive({
+req(input$update_plot3)
+      ### Plot Clusters
+  myClusters <-which(clusters$cluster==input$update_plot3)
+      
+      #### customize clusplot
+      plot_data <- clusplot(bd[myClusters,], clusters$cluster[myClusters], 
+                            color=T, shade=F,
+                            labels=0,lines=0, # display cluster labels
+                            main="k-Means Cluster Analysis", 
+                            xlab = "Pop Practicing Open Defecation",
+                          ylab = "Deaths by Conditions")
   })
   
   
   #### Render the plot
-  output$plot <- renderPlot({
-    plot_data()
+  output$plot2 <- renderPlot({
+    plot_data2()
+  })
+  
+  output$plot3 <- renderPlot({
+    plot_data3()
   })
   
 })
